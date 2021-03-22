2c85589daf666b8200335e6e0171fc1ecf8ff8d7
Arthur Passcal <apascal002@u-bordeaux.fr>
2021-03-22 13:58:58 +0100

    enum + classse position

---
+++ src/gomoku/Position.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gomoku;
+
+/**
+ *
+ * @author Arthur & Wijdan
+ */
+public class Position {
+    int paireX;
+    int paireY;
+    
+    public Position (int X, int Y) {
+    this.paireX = X;
+    this.paireY = Y;
+    
+} 
+}
