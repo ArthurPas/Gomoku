2287d83942827e231b6aa1b524f3e03075e098cd
Arthur Passcal <arthur.pascal@etu.u-bordeaux.fr>
2021-03-25 09:55:50 +0100

    exceptions squelette

---
+++ src/gomoku/Utils.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gomoku;
+
+/**
+ *
+ * @author apascal002
+ */
+public class Utils {
+    
+    public int hexaVersInt(char lettre){
+        return Integer.parseInt(String.valueOf(lettre));
+    }
+}
