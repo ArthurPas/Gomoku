2581ed7e8e48fd1870157b5a42ba8262558f7daa
Wijdan Abderrahmani <wabderrahman@IUT.BX1>
2021-03-25 09:59:19 +0100

    maj

--- src/gomoku/Gomoku.java
+++ src/gomoku/Gomoku.java
@@ -15,19 +15,18 @@ public class Gomoku {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("----------------------------------"); 
+        System.out.println("              Gomoku              "); 
+        System.out.println("----------------------------------"); 
+        System.out.println("              Bienvenue           "); 
+        //joueur.joueur
+        Plateau.plateau();
     }
-    
-    public static void plateau() {
-        // TODO code application logic here
-    }
+
+     public static void x(){
      
+     }
  
      
      
 }
-
-
-//etape creer deux joueurs
-// etape
-//etape une plateau
--- src/gomoku/Plateau.java
+++ src/gomoku/Plateau.java
@@ -5,11 +5,14 @@
  */
 package gomoku;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Arthur & Wijdan 
  */
 public class Plateau {
+   
     public void set (Position p, Couleur c){
         //TODO
     }
@@ -17,4 +20,72 @@ public class Plateau {
         return null;
         //TODO
     }
+    
+     public static void plateau() {
+        choix();
+        tab();
+        
+    }
+     
+     public static void choix(){
+        String choix;        
+        Scanner clavier = new Scanner(System.in);
+        System.out.println("*--------------------------------*");
+        System.out.println("  Veuillez choisir votre plateau");
+        System.out.println("*--------------------------------*");
+        System.out.println("|          a) 10x5               |");
+        System.out.println("|          b) 15x10              |");
+        System.out.println("|          c) 20x20              |");
+        System.out.println("*--------------------------------*");
+        choix = clavier.nextLine();
+      
+        
+        if (choix.equals("a") || choix.equals("A")){
+            System.out.println("Vous avez sélectionné le plateau A [10x5]"); 
+            tableauA();
+        }
+        if (choix.equals("b")|| choix.equals("B")){
+            System.out.println("Vous avez sélectionné le plateau B [15x10]");
+            tableauB();
+        }
+        if (choix.equals("c")|| choix.equals("C")){
+            System.out.println("Vous avez sélectionné le plateau C [20x20]");
+            tableauC();
+        }
+     }
+        
+        public static void tableauA(){
+            
+        }
+        
+        public static void tableauB(){
+            
+        }
+  
+        
+        public static void tableauC(){
+        
+        }
+        
+         
+ 
+
+ 
+
+     public static void tab() {
+         String a = new String ("[ ]") ;
+         int cases[][] = new int[3][3];
+         
+         for (int i = 0; i < 10; i++) {
+             for (int j = 0; j < 1; j++ ) {
+//               
+                 System.out.println(a); 
+                 System.out.println("[ ][ ][ ][ ][ ]"); 
+                 System.out.println("[ ][ ][ ][ ][ ]"); 
+                 
+             }
+         }
+         
+     }
+
 }
